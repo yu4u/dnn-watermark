3c912e2007377ad7a537e35afbbb31dccd8a3bfa
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2022-04-16 18:11:55 +0900

    ローカルデータセットのパスを変数で指定するように変更する

--- docker/launch.sh
+++ docker/launch.sh
@@ -2,6 +2,7 @@
 
 TAG='funabiki/dnn-watermark_nvidia_cuda:8.0-cudnn5-devel-ubuntu16.04'
 PROJECT_DIR="$(cd "$(dirname "${0}")/.." || exit; pwd)"
+DATASET_DIR="${PROJECT_DIR}/../dataset/dnn-watermark/.keras"
 
 # build
 cd "$(dirname "${0}")/.." || exit
@@ -12,6 +13,6 @@ docker run -it --rm \
   --shm-size=8g \
   --gpus all \
   -v "${PROJECT_DIR}:/work" \
-  -v "${PROJECT_DIR}/../dataset/dnn-watermark/.keras:/root/.keras" \
+  -v "${DATASET_DIR}:/root/.keras" \
   -w "/work" \
   "${TAG}"
